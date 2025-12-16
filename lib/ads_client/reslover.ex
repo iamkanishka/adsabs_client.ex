3c09264ec8204a3cef91286d9ce97714f6b8a65a
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-12-16 21:33:00 +0530

    Rename AdsClient to AdsabsClient

--- lib/ads_client/reslover.ex
+++ lib/ads_client/reslover.ex
@@ -1,34 +1,25 @@
-defmodule AdsClient.Resolver do
+defmodule AdsabsClient.Resolver do
   @moduledoc """
   Resolver API for getting links to external resources.
   """
 
-  alias AdsClient.{HTTP, Error}
+  alias AdsabsClient.{HTTP, Error}
 
-  @link_types ~w(
-    abstract citations references coreads toc openurl metrics
-    graphics esource data inspire librarycatalog presentation
-    associated
-  )a
+  @link_types ~w(abstract citations references coreads toc openurl metrics
+                 graphics esource data inspire librarycatalog presentation
+                 associated)a
 
   @spec resolve(String.t(), atom() | nil, keyword()) ::
     {:ok, map()} | {:error, Error.t()}
   def resolve(bibcode, link_type \\ nil, opts \\ []) do
-    path = build_path(bibcode, link_type)
+    path = if link_type, do: "/resolver/#{bibcode}/#{link_type}", else: "/resolver/#{bibcode}"
 
     case HTTP.get(path, opts) do
-      {:ok, %{body: response}} ->
-        {:ok, response}
-      {:error, _} = error ->
-        error
+      {:ok, %{body: response}} -> {:ok, response}
+      {:error, _} = error -> error
     end
   end
 
   @spec list_link_types() :: list(atom())
   def list_link_types, do: @link_types
-
-  defp build_path(bibcode, nil), do: "/resolver/#{bibcode}"
-  defp build_path(bibcode, link_type) when is_atom(link_type) do
-    "/resolver/#{bibcode}/#{link_type}"
-  end
 end
