e268e7c4a6648e7c807e40377e5616f54902b807
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-12-16 21:33:35 +0530

    Rename AdsClient to AdsabsClient

--- lib/ads_client/libraries.ex
+++ lib/ads_client/libraries.ex
@@ -1,34 +1,33 @@
-defmodule AdsClient.Libraries do
+defmodule AdsabsClient.Libraries do
   @moduledoc """
   Libraries API for managing paper collections.
   """
 
-  alias AdsClient.{HTTP, Library, Error}
+  alias AdsabsClient.{HTTP, Library, Error}
 
   @spec list_libraries(keyword()) ::
-    {:ok, list(Library.t())} | {:error, Error.t()}
+          {:ok, list(Library.t())} | {:error, Error.t()}
   def list_libraries(opts \\ []) do
     case HTTP.get("/biblib/libraries", opts) do
       {:ok, %{body: %{"libraries" => libraries}}} ->
         {:ok, Enum.map(libraries, &Library.from_api/1)}
+
       {:error, _} = error ->
         error
     end
   end
 
   @spec get_library(String.t(), keyword()) ::
-    {:ok, Library.t()} | {:error, Error.t()}
+          {:ok, Library.t()} | {:error, Error.t()}
   def get_library(library_id, opts \\ []) do
     case HTTP.get("/biblib/libraries/#{library_id}", opts) do
-      {:ok, %{body: library}} ->
-        {:ok, Library.from_api(library)}
-      {:error, _} = error ->
-        error
+      {:ok, %{body: library}} -> {:ok, Library.from_api(library)}
+      {:error, _} = error -> error
     end
   end
 
   @spec create_library(String.t(), String.t(), keyword()) ::
-    {:ok, Library.t()} | {:error, Error.t()}
+          {:ok, Library.t()} | {:error, Error.t()}
   def create_library(name, description, opts \\ []) do
     body = %{
       "name" => name,
@@ -38,73 +37,44 @@ defmodule AdsClient.Libraries do
     }
 
     case HTTP.post("/biblib/libraries", body: body) do
-      {:ok, %{body: library}} ->
-        {:ok, Library.from_api(library)}
-      {:error, _} = error ->
-        error
+      {:ok, %{body: library}} -> {:ok, Library.from_api(library)}
+      {:error, _} = error -> error
     end
   end
 
   @spec add_documents(String.t(), list(String.t()), keyword()) ::
-    {:ok, map()} | {:error, Error.t()}
+          {:ok, map()} | {:error, Error.t()}
   def add_documents(library_id, bibcodes, opts \\ []) do
-    body = %{
-      "bibcode" => bibcodes,
-      "action" => "add"
-    }
+    body = %{"bibcode" => bibcodes, "action" => "add"}
 
     case HTTP.post("/biblib/documents/#{library_id}", body: body) do
-      {:ok, %{body: response}} ->
-        {:ok, response}
-      {:error, _} = error ->
-        error
-    end
-  end
-
-  @spec remove_documents(String.t(), list(String.t()), keyword()) ::
-    {:ok, map()} | {:error, Error.t()}
-  def remove_documents(library_id, bibcodes, opts \\ []) do
-    body = %{
-      "bibcode" => bibcodes,
-      "action" => "remove"
-    }
-
-    case HTTP.post("/biblib/documents/#{library_id}", body: body) do
-      {:ok, %{body: response}} ->
-        {:ok, response}
-      {:error, _} = error ->
-        error
+      {:ok, %{body: response}} -> {:ok, response}
+      {:error, _} = error -> error
     end
   end
 
   @spec delete_library(String.t(), keyword()) ::
-    {:ok, map()} | {:error, Error.t()}
+          {:ok, map()} | {:error, Error.t()}
   def delete_library(library_id, opts \\ []) do
     case HTTP.delete("/biblib/libraries/#{library_id}", opts) do
-      {:ok, %{body: response}} ->
-        {:ok, response}
-      {:error, _} = error ->
-        error
+      {:ok, %{body: response}} -> {:ok, response}
+      {:error, _} = error -> error
     end
   end
 end
 
-defmodule AdsClient.Library do
-  @moduledoc """
-  Library struct representing an ADS library.
-  """
-
+defmodule AdsabsClient.Library do
   @type t :: %__MODULE__{
-    id: String.t(),
-    name: String.t(),
-    description: String.t(),
-    num_documents: integer(),
-    date_created: String.t(),
-    date_last_modified: String.t(),
-    permission: String.t(),
-    public: boolean(),
-    owner: String.t()
-  }
+          id: String.t(),
+          name: String.t(),
+          description: String.t(),
+          num_documents: integer(),
+          date_created: String.t(),
+          date_last_modified: String.t(),
+          permission: String.t(),
+          public: boolean(),
+          owner: String.t()
+        }
 
   defstruct [
     :id,
