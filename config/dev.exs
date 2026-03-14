fe9195d4d2ba6172c29d0214d6420bc9b3f2a989
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-14 20:50:22 +0530

    Updated

--- config/dev.exs
+++ config/dev.exs
@@ -1,11 +1,4 @@
 import Config
 
 config :adsabs_client,
-  adapter: AdsabsClient.Adapter.Req,
-  base_url: "https://api.adsabs.harvard.edu/v1",
-  default_timeout: 30_000,
-  max_retries: 3,
-  retry_delay: 1_000,
-  backoff_multiplier: 2.0,
-  jitter: 0.1,
-  api_token: "1rX8x3Ly32XPMXvZ7nyQIKsTjsBFbLwJrHvFAwbG"
+  receive_timeout: 60_000
