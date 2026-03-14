8fa4b3e5c267e8fb1e67978c9a22a274dc22dcd3
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-14 20:46:37 +0530

    dialyzer_ignore.exs

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,7 @@
+
+# Dialyzer ignore patterns
+# Format: [{:warn_type, source_file_pattern, line_number_or_range}]
+[
+  # Ignore warnings from generated code in test support
+  # {:warn_return_no_exit, ~r/test\/support/, :_}
+]
